0a982de29a803c91a7f8061bfae1b7dcc2bd4eec
Malvino-creator <malvin.opondo@student.moringaschool.com>
2022-09-24 15:01:31 +0300

    First commit

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+export const AddTransaction = () => {
+const[date, setDate] = useState ('');
+const[description,setDescription] = useState (0);
+const[category,setCategory] = useState (1);
+const[number,setAmount] = useState (2);
+
+
+}
 
 function AddTransactionForm() {
   return (
@@ -6,7 +16,7 @@ function AddTransactionForm() {
       <form className="ui form">
         <div className="inline fields">
           <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
+          <input type="text" value={description} onChange={(e)=> setDescription(e.target.value)} name="description" placeholder="Description" />
           <input type="text" name="category" placeholder="Category" />
           <input type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
